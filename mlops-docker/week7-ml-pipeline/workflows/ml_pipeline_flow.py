0eb99384224bc66de6222eb3d8c540904dd4cda2
sowmi-kasthuri <sowmi.kasthuri@gmail.com>
2025-12-02 21:26:43 +0530

    Week7 Day2 - Prefect flow skeleton and initial run

--- mlops-docker/week7-ml-pipeline/workflows/ml_pipeline_flow.py
+++ mlops-docker/week7-ml-pipeline/workflows/ml_pipeline_flow.py
@@ -1,1 +1,33 @@
-# Week 7 Prefect flow placeholder
+# Week 7 Prefect flow outline
+from prefect import flow, task
+
+@task
+def load_data():
+    pass
+
+@task
+def validate_data(data):
+    pass
+
+@task
+def train_model(data):
+    pass
+
+@task
+def evaluate_model(model):
+    pass
+
+@task
+def register_model(model,metrics):
+    pass
+
+@flow
+def ml_pipeline():
+    d = load_data()
+    v = validate_data(d)
+    m = train_model(v)
+    e = evaluate_model(m)
+    register_model(m,e)
+
+if __name__ == "__main__":
+    ml_pipeline()
