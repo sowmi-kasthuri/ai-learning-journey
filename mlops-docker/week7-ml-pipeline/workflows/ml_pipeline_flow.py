a9067193f0b6b6b9077953e961591f7a19486d4a
sowmi-kasthuri <sowmi.kasthuri@gmail.com>
2025-12-03 21:46:29 +0530

    Week7 Day3 - data loading, validation, training and eval skeleton

--- mlops-docker/week7-ml-pipeline/workflows/ml_pipeline_flow.py
+++ mlops-docker/week7-ml-pipeline/workflows/ml_pipeline_flow.py
@@ -1,21 +1,43 @@
 # Week 7 Prefect flow outline
 from prefect import flow, task
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 @task
 def load_data():
-    pass
+    # read csv
+    df = pd.read_csv("week7-ml-pipeline/data/iris.csv")
+    return df
 
 @task
 def validate_data(data):
-    pass
+    # check columns
+    expected_cols = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+    if not all(col in data.columns for col in expected_cols):
+        raise ValueError("Missing required columns in dataset")
+    
+    # check nulls
+    if data.isnull().sum().sum() > 0:
+        raise ValueError("Dataset contains null values")
+    return data
 
 @task
 def train_model(data):
-    pass
+    X = data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+    y = data["species"]
+
+    model = LogisticRegression(max_iter=200)
+    model.fit(X,y)
+    return model
 
 @task
-def evaluate_model(model):
-    pass
+def evaluate_model(model,data):
+    X = data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+    y = data["species"]
+
+    accuracy = model.score(X, y)
+    return accuracy
+
 
 @task
 def register_model(model,metrics):
@@ -26,7 +48,7 @@ def ml_pipeline():
     d = load_data()
     v = validate_data(d)
     m = train_model(v)
-    e = evaluate_model(m)
+    e = evaluate_model(m,v)
     register_model(m,e)
 
 if __name__ == "__main__":
